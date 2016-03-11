989c833500b7122bba8eb717fed9e21c48a8ebe9
AnchorTeam <www.AnchorTM@gmail.com>
2016-03-11 13:52:58 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,9 +14,8 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
-  --[[ Checkout https://github.com/Josepdal/DBTeam
-  --GNU GPL v2 license.]]
+  return 'Anchor Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\n is Support Super groups\n\nDevelopers :\n@GorgTehran\n@Black_Hat_ADMIN1\n\n' 
+  --[[ https://telegram.me/@AnchorRobot]]
 end
 
 return {
